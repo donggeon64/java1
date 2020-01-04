d41777a77c2a7bded4afa7835a3d9335d51de8a2
Owner <Owner@DESKTOP-RO38BR2>
2020-01-04 16:46:39 +0900

    배열을 사용한 학점 계산

--- src/Basic.java
+++ src/Basic.java
@@ -3,7 +3,28 @@ public class Basic {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-     int x = 10, y= 20;
+		int[] rangeLimit = { 30, 40, 50, 60, 70, 80, 90, 100 };
+		String[] grade = { "F", "D", "C", "B", "A", "S", "SS", "SSS" };
+		int maxGradeLevel = grade.length-1;
+
+		int studenScore = 90;
+
+		int gradeLevel = 0;
+		String studentGrade = "SSS";
+
+		while ((studentGrade.equals("SSS")) && (gradeLevel < maxGradeLevel)) {
+
+			if (studenScore > rangeLimit[maxGradeLevel] || studenScore < 0) {
+				studentGrade = "측정불가 	";
+				break;
+
+			}
+			if (studenScore < rangeLimit[gradeLevel + 1]) {
+				studentGrade = grade[gradeLevel];
+			}
+			gradeLevel = gradeLevel + 1;
+		}
+		System.out.println(studentGrade);
 	}
 
 }
