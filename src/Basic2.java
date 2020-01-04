61afce4cbe8cc6927e038df07632264804024d78
Owner <Owner@DESKTOP-RO38BR2>
2020-01-04 18:46:49 +0900

    구구단 문제들

--- src/Basic2.java
+++ src/Basic2.java
@@ -1,20 +1,21 @@
 
 public class Basic2 {
-
+	
 	public static void main(String[] args) {
-
-		
-		//	//배열을 이용한 구구단 
+       
+       
+		// //배열을 이용한 구구단
 //		
 //	  int[] result =  new int[9];
 //	  for(int i = 2; i<result.length; i++) {
-//	      for(int j = 1; j<result.length; j++) {
-//	    	  System.out.println(i+"X"+j+"="+(i*j));
+//	      
+		// for(int j = 1; j<result.length; j++) {
+//	    	 
+		// System.out.println(i+"X"+j+"="+(i*j));
 //	      }
 //	  }
-	
-	
-	} 
-	
+
+		
+	}
 
 }
