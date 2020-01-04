264af7e37bd5a19eba63f90117485a1661d2d509
Owner <Owner@DESKTOP-RO38BR2>
2020-01-04 18:02:01 +0900

    배열을 이용한 구구단

---
+++ src/Basic2.java
@@ -0,0 +1,20 @@
+
+public class Basic2 {
+
+	public static void main(String[] args) {
+
+		
+		//	//배열을 이용한 구구단 
+//		
+//	  int[] result =  new int[9];
+//	  for(int i = 2; i<result.length; i++) {
+//	      for(int j = 1; j<result.length; j++) {
+//	    	  System.out.println(i+"X"+j+"="+(i*j));
+//	      }
+//	  }
+	
+	
+	} 
+	
+
+}
