9b80298b9a0fccf0c20846508b4b0c9123072707
Owner <Owner@DESKTOP-RO38BR2>
2020-01-04 17:32:11 +0900

    구구단 출력  while 문 for문 Scanner

---
+++ src/Basic1.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Basic1 {
+
+	public static void main(String[] args) {
+
+		/*
+		 * System.out.println("구구 단  중 출력할 단은 ? :"); 
+		 * Scanner scanner = new Scanner(System.in); int number = scanner.nextInt();
+		 * System.out.println(number*1); System.out.println(number*2);
+		 * System.out.println(number*3); System.out.println(number*4);
+		 * System.out.println(number*5); System.out.println(number*6);
+		 */
+/*	  //6단 
+		int i = 1;
+		while (i<10) {
+			System.out.println(6*i);
+			i=i+1;
+			
+		}
+		
+	    //7단 
+		for(int j = 1; j<10; j++) {
+			System.out.println(7*1);
+		}
+	
+*/  
+		/*
+		 * //8단 , 9단
+		 * 
+		 * System.out.println("구구단 중 출력할 단은 "); Scanner scanner = new
+		 * Scanner(System.in); int number = scanner.nextInt();
+		 * System.out.println("사용자가 입력한 값 : " + number);
+		 * 
+		 * if(number < 2) { System.out.println("값을 잘못 입력하였습니다 ");
+		 * 
+		 * }else if(number > 9 ) { System.out.println("값을 잘못 입력하였습니다");
+		 * 
+		 * }else { for(int i = 1; i <10; i++) { System.out.println(number* i); } }
+		 */
+		
+	}
+}
